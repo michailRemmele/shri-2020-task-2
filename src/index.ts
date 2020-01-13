58355fda688ac445c6e6cb41fdc3b93ab5b6131f
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-01-13 22:12:53 +0600

    added default export of global lint function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Linter, { Error } from './linter/linter';
 
 const linter = new Linter();
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function lint(json: string): Error[] {
   return linter.lint(json);
 }
+
+export default lint;
