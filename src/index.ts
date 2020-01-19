0225f6e9b2100c523ce1adfb431de7645679cd63
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-01-19 22:17:05 +0600

    added lint function export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import linterRules from './linterRules.json';
 
 const linter = new Linter(linterRules);
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function lint(json: string): Error[] {
   return linter.lint(json);
 }
+
+export default lint;
